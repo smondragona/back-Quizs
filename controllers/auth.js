1251770d5c8baf7c76495ac58cf7a052ecd092c5
sergio <smondragon4321@gmail.com>
2021-02-12 10:36:26 -0600

    Google Sing IN Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express');
 const Usuario = require('../models/usuario');
 const sbcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar.jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res) => {
 
@@ -50,8 +51,60 @@ const login = async (req, res) => {
 
 }
 
+const googleSingin = async ( req, res = response ) => {
 
+    const {id_token} = req.body;
+
+    try {
+    const {correo, nombre, img } = await googleVerify(id_token);
+
+   // console.log(googleUser);
+
+    let usuario = await Usuario.findOne({correo});
+
+    if(!usuario){
+        //Tengo que crear local
+        const data = {
+            nombre,
+            correo,
+            password: ':P',
+            img,
+            google: true
+        }
+
+        usuario = new Usuario(data);
+        await usuario.save();
+    }
+
+    //Si el usuario en DB
+    if( !usuario.estado){
+        return res.status(401).json({
+            msg: 'Hable con el administrador usuario bloqueado'
+        })
+    }
+        
+      /*   res.json({
+            msg: 'Todo OK! google singin'
+        }) */
+
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+    res.json({
+        usuario,
+        token
+    });
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({
+            msg: 'Token de Google no válido'
+        })
+    }
+
+    
+}
 
 module.exports = {
-    login
+    login,
+    googleSingin
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSingin } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -11,6 +11,11 @@ router.post('/login',[
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/google',[
+    check('id_token', 'El id_token es necesario.').not().isEmpty(),
+    validarCampos
+], googleSingin);
 
 
 
